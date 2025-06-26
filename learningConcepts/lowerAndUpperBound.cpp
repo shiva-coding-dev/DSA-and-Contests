2c66949ffd666f7b1db518401b3f59cf9e641002
Shiva Gupta <shivaiitpatna@gmail.com>
2025-06-26 12:33:25 +0530

    Add binary Search functions

--- learningConcepts/lowerAndUpperBound.cpp
+++ learningConcepts/lowerAndUpperBound.cpp
@@ -4,23 +4,28 @@
 # include<string>
 # include<algorithm>
 using namespace std;
+// This could be used for the finding the number in the sorted array or finding the count of any number finding the upper and the lower bound 
 
+/*
+eg lets take a sorted array 
+5,6,7,8,9,10,10,10,23,34,
+now i have to find whether 10 occurs if it does how many time 
+
+*/
 int main() {
-    vector<int> a;
-    a.push_back(12);
-    a.push_back(45);
-    a.push_back(56);
-    a.push_back(65);
-    a.push_back(98);
-    a.push_back(42);
-    a.push_back(53);
-    a.push_back(23);
-
+    vector<int> a = {5,6,7,8,9,10,10,10,23,34};
+    int n = a.size();
+    int target = 10;
     sort(a.begin() , a.end());
-    auto k = lower_bound(a.begin() , a.end() , 65) - a.begin();
-    if (k < a.size() && a[k] == 65) {
-        cout<<"x found at index "<<k<<"\n";
-    }   
-    cout<<endl;
+    auto k = lower_bound(a.begin() , a.end() , target) - a.begin();
+    auto l = upper_bound(a.begin() , a.end() , target) - a.begin();
+    if(k<n && a[k] == target)cout<<"Target found at the index = "<<k<<" and "<<l-1<<"\n";
+    //k and l are the indices of the target in the sorted array
+    //to find the count of the ten simply do 
+    cout<<"Count of the Target is - "<<l-k<<"\n";
+
+    //below code gives the same data it gives the count  , Here r.first if the lower_bound value and r.second is the upper_bound value 
+    auto r = equal_range(a.begin() , a.end() , target);
+    cout<<r.second-r.first<<"\n";
     return 0;
 }
