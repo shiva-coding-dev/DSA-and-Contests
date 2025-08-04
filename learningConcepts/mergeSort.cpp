3a33eedfefc94d6e8e9cb93f925bdaf60cad836e
Shiva Gupta <shivaiitpatna@gmail.com>
2025-08-05 00:59:07 +0530

    Save the Merge Sort algorithm

--- learningConcepts/mergeSort.cpp
+++ learningConcepts/mergeSort.cpp
@@ -1,9 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+void merge(vector<int> &a , int left ,int mid , int right ){
+    vector<int> temp ;
+    int i = left , j = mid +1;
+    //merge the sorted array 
+    while(i<=mid  && j<=right){
+        if(a[i]<=a[j]){
+            temp.push_back(a[i++]);
+        }else{
+            temp.push_back(a[j++]);
+        }
+    }
+    while (i <= mid) temp.push_back(a[i++]);
+    while (j <= right) temp.push_back(a[j++]);
+    for (int k = 0; k < temp.size(); k++)
+        a[left + k] = temp[k];
+}
+
+void mergeSort(vector<int> &a , int left , int right ){
+    if(left>=right)return;
+    int mid = (left+right)/2;
+    mergeSort(a, left , mid );
+    mergeSort(a, mid+1 , right );
+    merge(a , left , mid , right);
+
+}
 
 int main() {
     vector<int> a  = {1,2,34,54,65,65,34,45,68,88,3,223,34};
-    
+    mergeSort(a ,0,a.size()-1);
 
+    for(auto x:a)cout<<x<<" ";
+    cout<<endl;
     return 0;
 }
